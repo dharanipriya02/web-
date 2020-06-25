ba73eab14b923465ee03e746dfcf96435c12c14d
Ameeruddin <ameeruddin9848@gmail.com>
2020-06-25 18:35:24 +0530

    made changes

--- AdminTheme/maintheme/urls.py
+++ AdminTheme/maintheme/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
 
+	path('',views.login, name="login"),
     path("index",views.index, name="index"),
-    path('',views.login, name="login"),
+    
     path("add_sport",views.add_sport, name="add_sport"),
     path("edit_sport",views.edit_sport, name="edit_sport"),
     path("view_sport",views.edit_sport, name="view_sport"),
--- AdminTheme/maintheme/views.py
+++ AdminTheme/maintheme/views.py
@@ -4,9 +4,6 @@ import json
 #Create your views here.
 r=None
 p=None
-def index(request) :
-    return render(request,"index.html")
-   # response=request.post('http://ourlink.com/login',data={'username':user_name.'password':password_})
 
 
 
@@ -18,18 +15,19 @@ def login(request) :
        global r 
        global p
        try:
-           print("heyy")
-           r = requests.post('https://group-10-api.herokuapp.com/Adminlogin',data={'username':uname,'password':pas})
-           print("hello")
-           p= r.json()['access_token']
-       except ValueError:
-           print("hellooo")
+        r = requests.post('https://group-10-api.herokuapp.com/Adminlogin',data={'username':uname,'password':pas})
+        p= r.json()['access_token']
+        return render(request,'index.html')
+       except KeyError:
+        return render(request, "login.html")
 
-            
-
-       if(p!=None):
-        	return render(request,"index.html",{})
    return render(request, "login.html")
+
+
+def index(request) :
+    return render(request,"index.html")
+   # response=request.post('http://ourlink.com/login',data={'username':user_name.'password':password_})
+
 def add_sport(request) :
     return render(request, "add_sport.html")
 
