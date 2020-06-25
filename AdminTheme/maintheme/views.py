30134601bfee04971cf3c48a1601a63a78a97a8e
dharanipriya02 <dharanipriya0002@gmail.com>
2020-06-25 17:12:30 +0530

    commit

--- AdminTheme/maintheme/views.py
+++ AdminTheme/maintheme/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+import requests
+import json
+#Create your views here.
+r=None
+p=None
 def index(request) :
-    return render(request, "index.html")
+    return render(request,"index.html")
+   # response=request.post('http://ourlink.com/login',data={'username':user_name.'password':password_})
+
+
 
 def login(request) :
-    return render(request, "login.html")
+   if (request.method)=="POST":
+       uname=request.POST.get('username') 
+       pas=request.POST.get('password')
+       rme=request.POST.get('customCheck')
+       global r 
+       global p
+       try:
+           print("heyy")
+           r = requests.post('https://group-10-api.herokuapp.com/Adminlogin',data={'username':uname,'password':pas})
+           print("hello")
+           p= r.json()['access_token']
+       except ValueError:
+           print("hellooo")
 
+            
+
+       if(p!=None):
+        	return render(request,"index.html",{})
+   return render(request, "login.html")
 def add_sport(request) :
     return render(request, "add_sport.html")
 
