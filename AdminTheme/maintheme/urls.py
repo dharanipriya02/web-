1f0e069ae1a5007e1e1b499b0a759d5ce6bef0f1
dharanipriya02 <dharanipriya0002@gmail.com>
2020-06-28 00:00:49 +0530

    Done with endpoint

--- AdminTheme/maintheme/urls.py
+++ AdminTheme/maintheme/urls.py
@@ -10,6 +10,21 @@ urlpatterns = [
     path("add_sport",views.add_sport, name="add_sport"),
     path("edit_sport",views.edit_sport, name="edit_sport"),
     path("view_sport",views.view_sport, name="view_sport"),
+    path("add_schedule",views.add_schedule, name="add_schedule"),
+    path("edit_schedule",views.edit_schedule, name="edit_schedule"),
+    path("edit_registration",views.edit_registration, name="edit_registration"),
+    path("view_registration",views.view_registration, name="view_registration"),
+    path("view_schedule",views.view_schedule, name="view_schedule"),
+    path("add_registration",views.add_registration, name="add_registration"),
+
+
+
+
+    
+
+
+
+
     
 
 ]
--- AdminTheme/maintheme/views.py
+++ AdminTheme/maintheme/views.py
@@ -26,7 +26,7 @@ def login(request) :
 
 def index(request) :
     return render(request,"index.html")
-   # response=request.post('http://ourlink.com/login',data={'username':user_name.'password':password_})
+   
 
 def add_sport(request) :
   if(p!=None):
@@ -36,8 +36,10 @@ def add_sport(request) :
          cat=request.POST.get('Sport_category') 
          mini=request.POST.get('min_team_size') 
          maxi=request.POST.get('max_team_size') 
+         gender=request.POST.get('gender') 
+
          print(Sport_id,mini)
-         a = requests.post('https://group-10-api.herokuapp.com/sportdetails',headers={'Authorization':f'Bearer {p}'},data={'sport_id':Sport_id,'sport_name':Sport_name,'sport_category':cat,'mini_team_size':mini,'max_team_size':maxi})
+         a = requests.post('https://group-10-api.herokuapp.com/sportdetails',headers={'Authorization':f'Bearer {p}'},data={'sport_id':Sport_id,'sport_name':Sport_name,'sport_category':cat,'mini_team_size':mini,'max_team_size':maxi,'gender':gender})
          print(a)
          return render(request,'add_sport.html')
     else:
@@ -45,20 +47,165 @@ def add_sport(request) :
   else:
     return render(request,"login.html")
 
-def edit_sport(request) :
-    return render(request, "edit_sport.html")
 
-def view_sport(request) :
+def edit_sport(request) :
   if(p!=None):
-    b = requests.get('https://group-10-api.herokuapp.com/sportdetails',headers={'Authorization':f'Bearer {p}'},data={'sport_name':'carroms'})
-    carroms = b.json()
-    d = requests.get('https://group-10-api.herokuapp.com/sportdetails',headers={'Authorization':f'Bearer {p}'},data={'sport_name':'football'})
-    football = d.json()
-    return render(request, "view_sport.html",{'carroms':carroms[0],'football':football[0]})
+    if (request.method)=="POST":
+
+         sport_name=request.POST.get('sport_name') 
+         start_date=request.POST.get('start_date') 
+         end_date=request.POST.get('end_date') 
+         print(sport_name,start_date,end_date)
+         a = requests.post('https://group-10-api.herokuapp.com/add_dates',headers={'Authorization':f'Bearer {p}'},data={'sport_name':sport_name,'start_date':start_date,'end_date':end_date})
+         print(a)
+         return render(request,'edit_sport.html')
+    else:
+        return render(request,'edit_sport.html')
   else:
     return render(request,"login.html")
 
+    #return render(request, "edit_sport.html")
 
+def view_sport(request) :
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         
+         sport_category=request.POST.get('sport_category')
+
+         a = requests.get('https://group-10-api.herokuapp.com/sport_category',headers={'Authorization':f'Bearer {p}'},data={'sport_category':sport_category})
+         sc = a.json()
+
+         print(sc)
+         return render(request,'view_sport.html',{'sc':sc})
+    else:
+        return render(request,'view_sport.html',{'sc':"False"})
+  else:
+    return render(request,"login.html")
+
+def add_schedule(request) :
+  if(p!=None):
+    if (request.method)=="POST":
+        
+         sport_name=request.POST.get('sport_name') 
+         team1_id=request.POST.get('team1_id') 
+         team2_id=request.POST.get('team2_id') 
+         match_date=request.POST.get('match_date') 
+         match_title=request.POST.get('match_title')
+         start_time=request.POST.get('start_time') 
+         reporting_time=request.POST.get('reporting_time') 
+         branch1=request.POST.get('branch1') 
+         branch2=request.POST.get('branch2') 
+
+         a = requests.post('https://group-10-api.herokuapp.com/add_schedule',headers={'Authorization':f'Bearer {p}'},data={'sport_name':sport_name,'team1_id':team1_id,'team2_id':team2_id,'match_date':match_date,'match_title':match_title,'start_time':start_time,'reporting_time':reporting_time,'branch1':branch1,'branch2':branch2})
+         print(a)
+         return render(request,'add_schedule.html')
+    else:
+        return render(request,'add_schedule.html')
+  else:
+    return render(request,"login.html")
+
+def edit_schedule(request):
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         team1_id=request.POST.get('team1_id') 
+         team2_id=request.POST.get('team2_id') 
+         match_date=request.POST.get('match_date') 
+         match_title=request.POST.get('match_title')
+         start_time=request.POST.get('start_time') 
+
+         reporting_time=request.POST.get('reporting_time') 
+      #   branch1=request.POST.get('branch1') 
+       #  branch2=request.POST.get('branch2') 
+
+         a = requests.post('https://group-10-api.herokuapp.com/Modify_schedule',headers={'Authorization':f'Bearer {p}'},data={'team1_id':team1_id,'team2_id':team2_id,'match_date':match_date,'match_title':match_title,'start_time':start_time,'reporting_time':reporting_time})
+         print(a)
+         return render(request,'edit_schedule.html')
+    else:
+        return render(request,'edit_schedule.html')
+  else:
+    return render(request,"login.html")
+
+def view_schedule(request):
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         
+         sport_name=request.POST.get('sport_name')
+
+         a = requests.get('https://group-10-api.herokuapp.com/schedule',headers={'Authorization':f'Bearer {p}'},data={'sport_name':sport_name})
+         sc = a.json()
+
+         print(sc)
+         return render(request,'view_schedule.html',{'sc':sc})
+    else:
+        return render(request,'view_schedule.html',{'sc':"False"})
+  else:
+    return render(request,"login.html")
+
+  
+
+def edit_registration(request):
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         team_name=request.POST.get('team_name') 
+         
+
+         a = requests.post('https://group-10-api.herokuapp.com/team_details',headers={'Authorization':f'Bearer {p}'},data={'team_name':team_name})
+         print(a)
+         return render(request,'edit_registration.html')
+    else:
+        return render(request,'edit_registration.html')
+  else:
+    return render(request,"login.html")
+
+def view_registration(request):
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         
+         sport_name=request.POST.get('sport_name')
+         team_name=request.POST.get('team_name')
+
+
+         a = requests.get('https://group-10-api.herokuapp.com/team_members',headers={'Authorization':f'Bearer {p}'},data={'sport_name':sport_name,'team_name':team_name})
+         sc = a.json()
+
+         print(sc)
+         return render(request,'view_registration.html',{'sc':sc})
+    else:
+        return render(request,'view_registration.html',{'sc':"False"})
+  else:
+    return render(request,"login.html")
+
+def add_registration(request):
+  if(p!=None):
+    if (request.method)=="POST":
+      #   Sport_id=request.POST.get('Sport_id') 
+       #  Sport_name=request.POST.get('Sport_name') 
+         
+         sport_name=request.POST.get('sport_name')
+         #team_name=request.POST.get('team_name')
+
+
+         a = requests.get('https://group-10-api.herokuapp.com/team_details',headers={'Authorization':f'Bearer {p}'},data={'sport_name':sport_name})
+         sc = a.json()
+
+         print(sc)
+         return render(request,'add_registration.html',{'sc':sc})
+    else:
+        return render(request,'add_registration.html',{'sc':"False"})
+  else:
+    return render(request,"login.html")
+
+    
 
 
 
